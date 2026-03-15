59ab089c7508ce941307cb235e43076e185406b9
anishkakoudgave-afk <anishkakoudgave@gmail.com>
2026-03-15 12:02:49 -0700

    Add files via upload

---
+++ dogbreed.py
@@ -0,0 +1,19 @@
+class Dog:
+    species = "Canine"   
+
+    def __init__(self, name, breed):   
+        self.name = name
+        self.breed = breed
+
+    def display(self):
+        print("Name:", self.name)
+        print("Breed:", self.breed)
+        print("Species:", Dog.species)
+        print()
+
+
+dog1 = Dog("Buddy", "Labrador")
+dog2 = Dog("Max", "German Shepherd")
+
+dog1.display()
+dog2.display()
